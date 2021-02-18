f14a89abbb6b0b39b8e876c35adb8b58a0206206
Mateusz Kus <mat.k.view@gmail.com>
2021-02-18 11:28:57 +0100

    Added reset function and local storage
    Added function that resets timers. Applied local storage saving while adding and removing timers. Improved app scalability.

--- src/components/Timers.vue
+++ src/components/Timers.vue
@@ -11,7 +11,8 @@
     <div id="list">
       <ul>
         <li v-for="timer in dead_timers" :key="timer.value">
-          {{ timer.time.seconds }} <button>restart</button>
+          {{ timer.time.seconds }}
+          <button v-on:click="restart_timer(timer)">restart</button>
 
           <button v-on:click="delete_timer(timer)">x</button>
         </li>
@@ -42,10 +43,25 @@ export default {
   mounted() {
     this.$options.timer = window.setTimeout(this.update_time, 1000);
     this.date_now = Date.now();
+    if (localStorage.getItem("timers_values")) {
+      try {
+        this.timers_values = JSON.parse(localStorage.getItem("timers_values"));
+      } catch (e) {
+        localStorage.removeItem("timers_values");
+      }
+    }
+  },
+  beforeDestroy() {
+    this.save_data();
   },
   methods: {
+    /**
+     * Takes value of time and counts the difference betwen it and current time.
+     * Returns a dictionary with days, hours, minutes, seconds, and done - bool value if time is greather than current date.
+     * @param {int} timer_value - value of time in ms.
+     */
     diff(timer_value) {
-      var diff = Date.parse(timer_value) - this.date_now;
+      var diff = timer_value - this.date_now;
       let done = diff < 0;
       let d = Math.floor(diff / 86400000);
       diff = diff - d * 86400000;
@@ -56,45 +72,76 @@ export default {
       let s = Math.floor(diff / 1000);
       return { days: d, hours: h, minutes: m, seconds: s, done: done };
     },
+    /**
+     * Adds new timer with value of variable data_input_value
+     */
     add_new_timer() {
-      if (
-        !this.timers_values.some(
-          timer => timer.value == this.date_input_value
-        ) &&
-        Date.parse(this.date_input_value) - this.date_now > 0
-      ) {
+      let value = Date.parse(this.date_input_value);
+      let time = value - this.date_now;
+      if (!this.timers_values.some(timer => timer.value == value) && time > 0) {
         this.timers_values.push({
-          value: this.date_input_value,
-          time: this.diff(this.date_input_value)
+          value: value,
+          time: time
         });
+        this.save_data();
       }
     },
+    /**
+     * Removes provided timer from a list of timers.
+     * @param {object} timer - timer object to remove.
+     */
     delete_timer(timer) {
       let index = this.timers_values.findIndex(x => x.value === timer.value);
-      console.log(index);
       if (index > -1) {
         this.timers_values.splice(index, 1);
+        this.save_data();
       }
     },
+    /**
+     * Resets timer time by setting it's value as sum of it's time property to current time.
+     * @param {object} timer - timer object to reset.
+     */
+    restart_timer(timer) {
+      let index = this.timers_values.findIndex(x => x.value === timer.value);
+      if (index > -1) {
+        this.timers_values[index].value = this.date_now + timer.time;
+      }
+    },
+    /**
+     * Every second updates value of current time.
+     */
     update_time() {
       this.date_now = Date.now();
       this.$options.timer = window.setTimeout(this.update_time, 1000);
+    },
+    /**
+     * Saves list of timers values to local storage.
+     */
+    save_data() {
+      const parsed = JSON.stringify(this.timers_values);
+      localStorage.setItem("timers_values", parsed);
     }
   },
   computed: {
-    now() {
-      return Date.now();
-    },
+    /**
+     * Returns list of timers with additional remaining property provided by diff method.
+     */
     timers() {
       return this.timers_values.map(x => {
         return { ...x, remaining: this.diff(x.value) };
       });
     },
+    /**
+     * Returns list of timers which are still running.
+     */
     running_timers() {
       return this.timers.filter(x => {
         return x.remaining.done === false;
       });
     },
+    /**
+     * Returns list of timers which value is lower than current time.
+     */
     dead_timers() {
       return this.timers.filter(x => {
         return x.remaining.done === true;
